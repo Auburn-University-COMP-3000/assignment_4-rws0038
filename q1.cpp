f11cf431ed6467a57f49e5f4023b2fc5ffabf946
rws0038 <89710014+rws0038@users.noreply.github.com>
2021-11-07 18:25:44 -0600

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -5,36 +5,56 @@ using namespace std;
 
 class GasPump {
 	private:
-		double amountDispensed,
-		       amountCharged,
-		       costPerGallon;
-
+		double amountDispensed, amountCharged, costPerGallon, totalDispensed, totalCharged;
 	public:
-		double getAmountDispensed() { return amountDispensed; }
-		double getAmountCharged() { return amountCharged; }
-		double getCostPerGallon() { return costPerGallon; }
-		void setCostPerGallon(double cpg) { costPerGallon = cpg; }
-        
-		void dispense(int seconds) {
+		double cost, time;
+		double getAmountDispensed() { 
+			amountDispensed = 0.10 * time;
+			return amountDispensed;
+		}
+		double getAmountCharged() {
+			amountCharged = cost * getAmountDispensed();
+			return amountCharged;
+	    }
+		double getCostPerGallon() {
+			costPerGallon = cost; 
+			return costPerGallon; 
+			}
+		double getTotalDispensed() {
+			totalDispensed = totalDispensed + amountDispensed;
+			return totalDispensed;
 
+		}
+		double getTotalCharged() {
+			totalCharged = totalCharged + amountCharged;
+			return totalCharged;
 
 			
-			amountCharged = amountDispensed * costPerGallon;
 		}
 		void reset() {
-            
 			amountDispensed = 0.0;
 			amountCharged = 0.0;
 		}		
 };
 	
 int main() {
-	
-    //Define variables
-
-    // Define Class Object
-
-    // Call Functions
+	GasPump gas;
+	char dispenseAgain = 'x';
+  
+	do {
+		cout << "Enter the cost of gas per gallon: $";
+		cin >> gas.cost;
+		cout << "Enter how long the gas was pumped in seconds: ";
+		cin >> gas.time;
+		cout << "Amount of gas dispensed: " << fixed << setprecision(2) << gas.getAmountDispensed() << " gallons" << endl;
+		cout << "Amount charged for gas: $" << fixed << setprecision(2) << gas.getAmountCharged() << endl;
+		cout << "Cost per gallon: $" << fixed << setprecision(2) << gas.getCostPerGallon() << endl;
+		cout << "Total amount of gas dispensed: "<< fixed << setprecision(2) << gas.getTotalDispensed() << " gallons" << endl;
+		cout << "Total amount charged for gas: $" << fixed << setprecision(2) << gas.getTotalCharged() << endl;
+		cout << "Enter Y to dispense again: ";
+		cin >> dispenseAgain;
+	}
+	while(dispenseAgain == 'Y');
 	
 
 	return 0;
