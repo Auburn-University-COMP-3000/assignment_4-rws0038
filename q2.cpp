fcd8abea2781f0188597b33dac398fc1224eb648
rws0038 <89710014+rws0038@users.noreply.github.com>
2021-11-07 18:26:10 -0600

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -4,18 +4,39 @@ using namespace std;
 
 class Temperature {
 	private:
-		double degreesKelvin,
-		       degreesFahrenheit,
-		       degreesCelsius;
+		double kelvin, fahrenheit, celsius;
+		       
 	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+		void setTempKelvin(double degreesKelvin) { 
+			kelvin = degreesKelvin;
+		}
+		double getTempKelvin() {
+			return kelvin;
+	    }
+		void setTempCelsius() {  
+			celsius = kelvin - 273.15;
+		}
+		double getTempCelsius() { 
+			return celsius;
+		 } 
+		void setTempFahrenheit() { 
+			fahrenheit = celsius * 9.0 / 5.0 + 32;
+		 }
+		double getTempFahrenheit() {
+			return fahrenheit;
+		}
 };
 
 int main() {
-
+	Temperature temp;
+	double kelv;
+	cout << "Enter a temperature in Kelvin: ";
+	cin >> kelv;
+	temp.setTempKelvin(kelv);
+	temp.setTempCelsius();
+	temp.setTempFahrenheit();
+	cout << "Temperature in Kelvin: " << temp.getTempKelvin() << " Kelvins" << endl;
+	cout << "Temperature in Celsius: " << temp.getTempCelsius() << " degrees Celsius" << endl;
+	cout << "Temperature in Fahrenheit: " << temp.getTempFahrenheit() << " degrees Fahrenheit" << endl;
+	return 0;
 }
